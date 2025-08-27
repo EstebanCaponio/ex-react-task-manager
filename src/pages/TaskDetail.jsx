2d3a54b5bd5878790e64217941a36df246462676
EstebanCaponio <estebancaponio@gmail.com>
2025-08-27 17:37:36 +0200

    feat: refactoring

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,38 +1,3 @@
-// import { useContext } from "react";
-// import { useParams } from "react-router-dom"
-// import { GlobalContext } from "../context/GlobalContext";
-
-// export default function TaskDetail() {
-
-//     const { id } = useParams();
-//     const { tasks } = useContext(GlobalContext);
-
-//     const task = tasks.find(t => t.id === parseInt(id));
-
-//     if (!task) {
-//         return (
-//             <h2 style={{ textAlign: "center" }}>task non trovata</h2>
-//         )
-//     }
-
-//     const handleDelete = () => {
-//         console.log('task da eliminare', task.id);
-//     }
-
-//     return (
-//         <>
-//             <div>
-//                 <h1>Dettaglio Task:</h1>
-//                 <p><strong>Titolo:</strong> {task.title}</p>
-//                 <p><strong>Descrizione:</strong> {task.description}</p>
-//                 <p><strong>Stato:</strong> {task.status}</p>
-//                 <p><strong>Data:</strong> {new Date(task.createdAt).toLocaleDateString()}</p>
-//                 <button onClick={handleDelete}>Elimina</button>
-//             </div>
-//         </>
-//     )
-// }
-
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { GlobalContext } from "../context/GlobalContext";
@@ -74,12 +39,10 @@ export default function TaskDetail() {
                 <h1>Dettaglio Task:</h1>
                 <p><strong>Titolo:</strong> {task.title}</p>
                 <p><strong>Descrizione:</strong> {task.description}</p>
-                {/* Nuova struttura per allineare stato e badge */}
                 <div className="task-status-line">
                     <p><strong>Stato:</strong></p>
                     <span className={`status-badge ${getStatusClass(task.status)}`}>{task.status}</span>
                 </div>
-                {/* Fine nuova struttura */}
                 <p><strong>Data:</strong> {new Date(task.createdAt).toLocaleDateString()}</p>
                 <button onClick={handleDelete} className="delete-button">Elimina</button>
             </div>
