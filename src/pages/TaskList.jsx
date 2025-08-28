6f88e6b9bb6d6d49db9918a45009b72eaefd7554
EstebanCaponio <estebancaponio@gmail.com>
2025-08-28 11:44:29 +0200

    feat: finish milestone 11

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useMemo, useState } from "react"
 import { GlobalContext } from "../context/GlobalContext"
 import TaskRow from "../components/TaskRow";
 
@@ -7,6 +7,40 @@ export default function TaskList() {
     const { tasks } = useContext(GlobalContext);
     console.log('tasks:', tasks)
 
+    const [sortBy, setSortBy] = useState('createdAt');
+    const [sortOrder, setSortOrder] = useState(1);
+
+    const sortIcon = sortOrder === 1 ? '⭣' : '⭡'
+
+    const handleSort = (field) => {
+        if (sortBy === field) {
+            setSortOrder(prev => prev * -1);
+        } else {
+            setSortBy(field);
+            setSortOrder(1);
+        }
+    }
+
+    const sortedTask = useMemo(() => {
+        return [...tasks].sort((a, b) => {
+            let comparison;
+
+            if (sortBy === 'title') {
+                comparison = a.title.localeCompare(b.title)
+            } else if (sortBy === 'status') {
+                const statusOptions = ['To do', 'Doing', 'Done']
+                comparison = statusOptions.indexOf(a.status) - statusOptions.indexOf(b.status)
+            } else if (sortBy === 'createdAt') {
+                const dateA = new Date(a.createdAt).getTime();
+                const dateB = new Date(b.createdAt).getTime();
+                comparison = dateA - dateB;
+            }
+
+            return comparison * sortOrder
+        })
+    }, [tasks, sortBy, sortOrder]);
+
+
     return (
         <>
             <h2>Lista delle tasks</h2>
@@ -14,18 +48,24 @@ export default function TaskList() {
                 <table>
                     <thead>
                         <tr>
-                            <th>nome</th>
-                            <th>status</th>
-                            <th>data creazione</th>
+                            <th onClick={() => handleSort('title')}>
+                                nome {sortBy === 'title' && sortIcon}
+                            </th>
+                            <th onClick={() => handleSort('status')} >
+                                status {sortBy === 'status' && sortIcon}
+                            </th>
+                            <th onClick={() => handleSort('createdAt')} >
+                                data creazione {sortBy === 'createdAt' && sortIcon}
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        {tasks.map(task => (
+                        {sortedTask.map(task => (
                             <TaskRow key={task.id} task={task} />
                         ))}
                     </tbody>
-                </table>
-            </div>
+                </table >
+            </div >
         </>
     )
 }
